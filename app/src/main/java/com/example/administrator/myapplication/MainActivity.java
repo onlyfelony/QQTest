a020d1372cf6af1c598a13c6ccce42bacac27f06
unknown <1260675642@qq.com>
2016-04-22 13:37:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView msgListView;
+    private EditText inputText;
+    private Button send;
+    private MsgAdapter adapter;
+    private List<Msg> msgList = new ArrayList<Msg>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initMsgs();
+        adapter = new MsgAdapter(MainActivity.this, R.layout.msg_item, msgList);
+        inputText = (EditText) findViewById(R.id.input_text);
+        send = (Button) findViewById(R.id.send);
+        msgListView = (ListView) findViewById(R.id.msg_list_view);
+        msgListView.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = inputText.getText().toString();
+                if (!"".equals(content)) {
+                    Msg msg = new Msg(content, Msg.TYPE_SENT);
+                    msgList.add(msg);
+                    adapter.notifyDataSetChanged();
+
+                    msgListView.setSelection(msgList.size());
+
+                    inputText.setText("");
+                }
+            }
+        });
+
+
+
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+    setSupportActionBar(toolbar);
+
+
+}
+
+    private void initMsgs() {
+        Msg msg1 = new Msg("Hello", Msg.TYPE_RECEIVED);
+        msgList.add(msg1);
+        Msg msg2 = new Msg("Hello. Who is that?", Msg.TYPE_SENT);
+        msgList.add(msg2);
+        Msg msg3 = new Msg("This is xuyaqin. ", Msg.TYPE_RECEIVED);
+        msgList.add(msg3);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/administrator/myapplication/ViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.administrator.myapplication;
+
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2016/4/21.
+ */
+public class ViewHolder {
+    LinearLayout leftLayout;
+    LinearLayout rightLayout;
+    TextView leftMsg;
+    TextView rightMsg;
+
+}
